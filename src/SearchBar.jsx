40a5cede457ea434f7144e2902cbe277107d6069
heliaakbari <heliaakbari@aut.ac.ir>
2024-04-24 10:06:19 +0330

    deleted search form

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,13 +1,43 @@
-import Accordion from 'react-bootstrap/Accordion';
-import SearchForm from './SearchForm';
+import { Button , Col,Row,Form,Accordion} from 'react-bootstrap';
+import { useState } from 'react';
+
 export default function SearchBar({setSearchInput}) {
+
+  const [id,setId]= useState("")
+  const [firstName,setFirstName]= useState("")
+  const [lastName,setLastName]=useState("")
+
+  function handleSearch(){
+    setSearchInput({firstName:firstName,lastName:lastName,id:id});
+  }
+
   return (
     <>
     <Accordion>
       <Accordion.Item eventKey="0">
         <Accordion.Header>جستجو</Accordion.Header>
         <Accordion.Body>
-          <SearchForm setSearchInput={setSearchInput}></SearchForm>
+              <Form >
+      <Row className="mb-3">
+        <Form.Group as={Col}>
+          <Form.Label>نام</Form.Label>
+          <Form.Control onChange={(e)=>setFirstName(e.target.value)} defaultValue={firstName} type="input" placeholder="نام را وارد کنید" />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>نام خانوادگی</Form.Label>
+          <Form.Control onChange={(e)=>setLastName(e.target.value)} defaultValue={lastName} type="input" placeholder="نام خانوادگی را وارد کنید" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridId">
+        <Form.Label>کد ملی</Form.Label>
+        <Form.Control onChange={(e)=>setId(e.target.value)} type="text" defaultValue={id} placeholder="کد ملی را وارد کنید" />
+      </Form.Group>
+        <div className="col-auto my-0  d-flex align-items-end ">
+      <Button className="btn btn-primary" onClick={handleSearch}>جستجو</Button>
+    </div>
+      </Row>
+    </Form>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
